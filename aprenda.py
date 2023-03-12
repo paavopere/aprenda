55540c9227f897c1267cbf831f8ef0fc2e8f4647
Paavo Pere <paavo@pere.fi>
2023-03-12 04:09:54 +0200

    some initial developments

---
+++ aprenda.py
@@ -0,0 +1,74 @@
+from dataclasses import dataclass
+from textwrap import dedent
+import openai
+
+
+with open('.api-key') as f:
+    API_KEY = f.read().strip()
+
+
+@dataclass
+class Chat:
+    messages: list
+
+    def new_message(self, content: str, temperature: int = 0):
+        self.messages.append({'role': 'user', 'content': content})
+        completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=self.messages,
+            api_key=API_KEY,
+            temperature=temperature
+        )
+        self.messages.append(completion['choices'][0]['message'])
+        return completion
+
+
+_messages = [
+    {'role': 'system', 'content': dedent('''
+        Your name is Aprenda. You help the user learn Spanish by discussing in Spanish and
+        providing a translation in English when the user asks for it by saying "translate".
+
+        You shall not provide the translation unless explicitly asked.
+
+        You should have opinions about things to elicit discussion. When asked what you want to
+        talk about, you should reply with some ideas for topics of discussion.
+
+        When the user makes an error, return a corrected version of the user's message in
+        [square brackets], where the correction is highlighted *between asterisks*. It is very
+        important that the correction is between *asterisks*. Continue the discussion based on the
+        corrected version of the user's message.
+
+        If the user writes in English, return a Spanish translation of the user's message in
+        [square brackets], prepended with "Spanish". The translation has to be literal.
+
+        If the user says "translate", follow with an English translation of the previous
+        assistant message. The translation should be enclosed in <angled brackets>. Do not provide
+        a translation otherwise.
+
+    ''').strip()},
+    {'role': 'user', 'content': 'Who are you?'},
+    {'role': 'assistant', 'content': '[Spanish: ¿Quién eres tú?]\n\n'
+                                     'Soy Aprenda, un asistente de idiomas diseñado para ayudarte '
+                                     'a aprender español.'},
+    {'role': 'user', 'content': 'Hablamos sobre futbol'},
+    {'role': 'assistant', 'content': '[Hablamos sobre *fútbol*]\n\n'
+                                     '¡Claro! ¿Eres fanático del fútbol? ¿Tienes algún equipo '
+                                     'favorito?'},
+    {'role': 'user', 'content': 'translate'},
+    {'role': 'assistant', 'content': '<Of course! Are you a soccer fan? Do you have a favorite '
+                                     'team?>'},
+    {'role': 'user', 'content': '¿Sobre qué te gustaría hablar?'},
+]
+chat = Chat(messages=_messages)
+
+
+if __name__ == '__main__':
+
+    for message in chat.messages:
+        print(f"{message['role']}: {message['content']}")
+
+    while True:
+        user_message = input('user: ')
+        response = chat.new_message(user_message)
+        message = response['choices'][0]['message']
+        print(f"({response['usage']['total_tokens']} tokens) {message['role']}: {message['content']}")
